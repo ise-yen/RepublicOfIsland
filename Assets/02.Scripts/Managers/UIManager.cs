d54d3dc88eb164d037b9600b91a7e0759113c054
Do Hyung Kim <wjp229@naver.com>
2020-07-19 14:23:29 +0900

    LevelUI

---
+++ Assets/02.Scripts/Managers/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    #region Ability Settings
+    public enum ABILITY { Tax, Dissatisfy, Comfort, Crime, Trade, Food }
+    public Transform[] abilityText;
+    #endregion
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void SetAbilityUI(ABILITY ability, string param)
+    {
+        abilityText[(int)ability].GetComponentInChildren<Text>().text = param;
+    }
+}
