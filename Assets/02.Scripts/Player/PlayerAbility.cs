6df8295d8f57f75e008be0afb5e9650331b443d2
Y-Wise <Y-Wise@users.noreply.github.com>
2020-08-09 14:48:14 +0900

    ADD 02. Scripts
    add Building, Event, Player, etc Scripts

---
+++ Assets/02.Scripts/Player/PlayerAbility.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbility : MonoBehaviour
+{
+
+    void Start()
+    {
+        //Debug.Log("[행정력/경제력/군사력] : " + administrativePower + "/" + economicPower + "/" + warMachine);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ObtainAbilitiy()
+    {
+
+    }
+}
