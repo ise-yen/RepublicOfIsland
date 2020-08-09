0bd4b3e43635567332ecbe3fff37764769c645ec
Y-Wise <Y-Wise@users.noreply.github.com>
2020-08-09 15:54:44 +0900

    Test Script
    Add Test Script which name is "CheckObj"

--- Assets/02.Scripts/Building/CourtBuilt.cs
+++ Assets/02.Scripts/Building/CourtBuilt.cs
@@ -14,12 +14,6 @@ public class CourtBuilt : MonoBehaviour
     public GameObject SecondCourt;
     public GameObject ThirdCourt;
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
