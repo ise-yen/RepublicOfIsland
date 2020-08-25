952fd929706d72617e2bcf8289e22ab612854340
Y-Wise <Y-Wise@users.noreply.github.com>
2020-08-26 05:44:46 +0900

    Building 수정
    ChurchBuilt, CourtBuilt

--- Assets/02.Scripts/Building/ChurchBuilt.cs
+++ Assets/02.Scripts/Building/ChurchBuilt.cs
@@ -13,82 +13,73 @@ public class ChurchBuilt : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            BuildFirstChurch();
-            BuildSecondChurch();
-            BuildThirdChurch();
+            RayHitting.instance.ClickObject();
+            if (RayHitting.instance.SelectObj != null)
+            {
+                Debug.Log("target: " + RayHitting.instance.SelectObj.name);
+                if (RayHitting.instance.SelectObj.name == "ChurchPlane")
+                {
+                    RayHitting.instance.SelectObj.SetActive(false);
+                    Debug.Log("0단계 눌렀습니다.");
+                    BuildFirstChurch();
+                }
+                if (RayHitting.instance.SelectObj.name == "Church1")
+                {
+                    Debug.Log("1단계 눌렀습니다.");
+                    BuildSecondChurch();
+                }
+                if (RayHitting.instance.SelectObj.name == "Church2")
+                {
+                    Debug.Log("2단계 눌렀습니다.");
+                    BuildThirdChurch();
+                }
+            }
             Debug.Log("교회 생성 완료");
         }
     }
 
     public void BuildFirstChurch()
     {
-        RayHitting.instance.ClickObject();
-        if (RayHitting.instance.SelectObj.name == "ChurchPlane")
-        {
-            //PlayStatManager.instance.Briket -= 125;
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -125);
-            Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -125);
+        Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
 
-            //KingAbilityManager.instance.AdministrativePower -= 400;
-            //UIManager.instance.SetAbilityUI(KingAbilityManager.instance.AdministrativePower, KingAbilityManager.instance.EconomicPower, KingAbilityManager.instance.WarPower);
-            PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -400);
-            Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
+        PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -400);
+        Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
 
-            //PlayStatManager.instance.Food -= 1;
-            //UIManager.instance.SetPlayerStat(UIManager.PLAYERSTAT.Food, PlayStatManager.instance.Food);
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -1);
-            Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -1);
+        Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
 
-            FirstChurch.SetActive(true);
-            Debug.Log("1단계 교회 생성");
-        }
+        FirstChurch.SetActive(true);
+        Debug.Log("1단계 교회 생성");
     }
     public void BuildSecondChurch()
     {
-        RayHitting.instance.ClickObject();
-        if (RayHitting.instance.SelectObj.name == "Church1")
-        {
-            //PlayStatManager.instance.Briket -= 250;
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -250);
-            Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
-          
-            //KingAbilityManager.instance.AdministrativePower -= 600;
-            //UIManager.instance.SetAbilityUI(KingAbilityManager.instance.AdministrativePower, KingAbilityManager.instance.EconomicPower, KingAbilityManager.instance.WarPower);
-            PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -600);
-            Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
-            
-            //PlayStatManager.instance.Food -= 3;
-            //UIManager.instance.SetPlayerStat(UIManager.PLAYERSTAT.Food, PlayStatManager.instance.Food);
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -3);
-            Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -250);
+        Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
 
-            FirstChurch.SetActive(false);
-            SecondChurch.SetActive(true);
-            Debug.Log("2단계 교회 생성");
-        }
+        PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -600);
+        Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
+
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -3);
+        Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
+
+        FirstChurch.SetActive(false);
+        SecondChurch.SetActive(true);
+        Debug.Log("2단계 교회 생성");
     }
     public void BuildThirdChurch()
     {
-        RayHitting.instance.ClickObject();
-        if (RayHitting.instance.SelectObj.name == "Church2")
-        {
-            //PlayStatManager.instance.Briket -= 500;
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -500);
-            Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
-            
-            //KingAbilityManager.instance.AdministrativePower -= 800;
-            //UIManager.instance.SetAbilityUI(KingAbilityManager.instance.AdministrativePower, KingAbilityManager.instance.EconomicPower, KingAbilityManager.instance.WarPower);
-            PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -800);
-            Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
-            
-            //PlayStatManager.instance.Food -= 5;
-            //UIManager.instance.SetPlayerStat(UIManager.PLAYERSTAT.Food, PlayStatManager.instance.Food);
-            PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -5);
-            Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Briket, -500);
+        Debug.Log("잔여 브리킷: " + PlayStatManager.instance.Briket);
 
-            SecondChurch.SetActive(false);
-            ThirdChurch.SetActive(true);
-            Debug.Log("3단계 교회 생성");
-        }
+        PlayerSystemManager.instance.ObtainKingAbility(PlayerSystemManager.KingAbility.AdministrativePower, -800);
+        Debug.Log("잔여 행정력: " + KingAbilityManager.instance.AdministrativePower);
+
+        PlayerSystemManager.instance.StatingStat(PlayerSystemManager.PlayState.Food, -5);
+        Debug.Log("잔여 식량: " + PlayStatManager.instance.Food);
+
+        SecondChurch.SetActive(false);
+        ThirdChurch.SetActive(true);
+        Debug.Log("3단계 교회 생성");
     }
 }
--- Assets/02.Scripts/RayHitting.cs
+++ Assets/02.Scripts/RayHitting.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class RayHitting : MonoBehaviour
 {
     public static RayHitting instance;
-    
+
     public RaycastHit hit; //충돌 감지
     public Ray ray;
-    private GameObject SelectObject; //클릭한 오브젝트
-    
-    //클릭한 오브젝트를 다른 스크립트에서 접근 가능하도록 세팅
+
+    GameObject selectObject; //클릭한 오브젝트
     public GameObject SelectObj {
-        get { return SelectObject;  }
-        set { SelectObject = value; }
+        get { return selectObject;  }
+        set { selectObject = value; }
     } //선택한 블럭
     
 
@@ -32,18 +31,16 @@ public class RayHitting : MonoBehaviour
 
     public void ClickObject()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //마우스의 위치를 레이로 변환
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //SelectObj = null;
+        //마우스의 위치를 레이로 변환
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            //레이 발사
-            if (Physics.Raycast(ray, out hit))
-            {
-                SelectObject = hit.collider.gameObject;
-                Debug.Log("--------------------------------");
-                Debug.Log("Object Name : " + hit.collider.gameObject.name);
-            }
+        //레이 발사
+        if (Physics.Raycast(ray, out hit))
+        {
+            SelectObj = hit.collider.gameObject;
+            Debug.Log("--------------------------------");
+            Debug.Log("Object Name : " + hit.collider.gameObject.name);
         }
     }
 }
