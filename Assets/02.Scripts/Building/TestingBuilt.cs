50e924cd544d2629d5805a3383d798582b784079
Y-Wise <Y-Wise@users.noreply.github.com>
2020-09-13 01:19:19 +0900

    ChurchBuilt / CourtBuilt 수정
    Raycasting없이 진화

---
+++ Assets/02.Scripts/Building/TestingBuilt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingBuilt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)){
+            ChurchBuilt.instance.BuildNextChurch();
+            ChurchBuilt.instance.UpgradeChurch();
+
+            CourtBuilt.instance.BuildNextCourt();
+            CourtBuilt.instance.UpgradeCourt();
+        }
+    }
+}
